39d41835577f248fdcb4b3aa31849446e159fc31
Thais Danieli <78539909+thaisdanieli@users.noreply.github.com>
2023-11-13 16:50:36 -0300

    Exercise043.py

--- PythonCursoEmVideo/Python 3 - Mundo 2/Exercise043.py
+++ PythonCursoEmVideo/Python 3 - Mundo 2/Exercise043.py
@@ -12,13 +12,11 @@ imc = peso/(altura**2)
 
 if imc < 18.5:
     print('Abaixo do Peso')
-elif imc >=18.5 and imc <=25:
+elif imc < 25:
     print('Peso ideal')
-#elif 30 > imc <=25:
-elif imc >=25 and imc <=30:
+elif imc < 30:
     print('Sobrepeso')
-#elif 40 > imc <= 30:
-elif imc >= 30 and imc <= 40:
+elif imc < 40:
     print('Obesidade')
 else:
     print('Obesidade mórbida!')
